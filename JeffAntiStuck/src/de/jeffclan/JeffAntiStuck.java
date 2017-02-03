9ad8920a76add72a6d4afd6232399c27403901a7
JEFF <jeff@jeffclan.de>
2017-02-03 14:12:53 +0100

    Update

--- JeffAntiStuck/src/de/jeffclan/JeffAntiStuck.java
+++ JeffAntiStuck/src/de/jeffclan/JeffAntiStuck.java
@@ -1,5 +1,6 @@
 package de.jeffclan;
 
+import org.bukkit.Color;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -7,33 +8,17 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class JeffAntiStuck extends JavaPlugin implements CommandExecutor {
-	
-    @Override
-    public void onEnable() {
-    	System.out.println("JeffAntiStuck aktiviert.");
-    }
-
-    @Override
-    public void onDisable() {
-    	System.out.println("JeffAntiStuck deaktiviert.");
-
-    }
-    
-    @Override
-    public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-    	
-        if(arg0 instanceof Player) {
-        	if(arg1.getName().equalsIgnoreCase("stuck")) {
-        		Player p = (Player) arg0;
-        		if(p.hasPermission("jeff.antistuck")) {
-        			p.teleport(p.getLocation().add(0, 0.2, 0));
-        			p.sendMessage("Du Tölpel...");
-        		}
-        	}
-        }
-    	
-    	return false;
-    }
-
-
+	@Override
+	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+		if (arg0 instanceof Player) {
+			if (arg1.getName().equalsIgnoreCase("stuck")) {
+				Player p = (Player) arg0;
+				if (p.hasPermission("jeff.antistuck")) {
+					p.teleport(p.getLocation().add(0, 0.2, 0));
+					p.sendMessage(Color.PURPLE+"Du Tölpel...");
+				}
+			}
+		}
+		return false;
+	}
 }
